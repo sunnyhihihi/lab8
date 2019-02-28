5812ea00e53be7116d2fe86848085ea077b0c41e
Sunny Sun <zes004@ucsd.edu>
2019-02-28 10:26:51 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'HsdAz9Hh1ht695m6bFmIy5AtP9F9em42'
+
+	var map = L.mapquest.map('map',{
+		center:[32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
